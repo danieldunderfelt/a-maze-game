96e6a7e3bd6f1a5fb2a954fc60ab689f60136980
Dunderfelt, Daniel <daniel.dunderfelt@mirumagency.com>
2015-05-09 20:56:34 +0300

    Line drawing moved to Cell

--- src/Cell.js
+++ src/Cell.js
@@ -1,3 +1,5 @@
+import PIXI from 'pixi.js'
+
 class Cell {
 
 	constructor(definition) {
@@ -5,13 +7,63 @@ class Cell {
 		this.actions = definition.actions || []
 		this.walls = definition.walls || [0, 0, 0, 0]
 		this.index = definition.index || 0
+
+		this.graphic = new PIXI.Graphics()
+		this.drawLengthFraction = 0
 	}
+
+	// Cell graphics rendering
+
+	draw(cellSize) {
+
+		this.renderLoc = {
+			x: this.loc.x * cellSize,
+			y: this.loc.y * cellSize,
+		}
+
+		this.drawWalls(cellSize)
+	}
+
+	drawWalls(cs) {
+		this.graphic.clear()
+
+		this.graphic.beginFill(0x000000, 0)
+		this.graphic.lineStyle(1.5, 0xffffff, 1)
+
+		this.graphic.moveTo(this.renderLoc.x, this.renderLoc.y)
+		if(this.walls[0] === 0) this.graphic.lineTo(this.renderLoc.x + cs, this.renderLoc.y)
+
+		this.graphic.moveTo(this.renderLoc.x + cs, this.renderLoc.y)
+		if(this.walls[1] === 0) this.graphic.lineTo(this.renderLoc.x + cs, this.renderLoc.y + cs)
+
+		this.graphic.moveTo(this.renderLoc.x + cs, this.renderLoc.y + cs)
+		if(this.walls[2] === 0) this.graphic.lineTo((this.renderLoc.x + cs) - cs, this.renderLoc.y + cs)
+
+		this.graphic.moveTo(this.renderLoc.x, this.renderLoc.y + cs)
+		if(this.walls[3] === 0) this.graphic.lineTo(this.renderLoc.x, (this.renderLoc.y + cs) - cs)
+
+		this.graphic.endFill()
+	}
+
+	getDrawlength(fullLength) {
+		return fullLength * this.drawLengthFraction
+	}
+
+	// Player-related callback hooks
 
 	onEnter() {
 
 	}
 
 	onLeave() {
+
+	}
+
+	onOccupied() {
+
+	}
+
+	onEmpty() {
 
 	}
 }
--- src/MainRenderer.js
+++ src/MainRenderer.js
@@ -11,74 +11,64 @@ export default class {
 		var canvasContainer = document.getElementById('gameArea')
 		this.screenSize = canvasContainer.offsetHeight
 
-		this.renderer = new PIXI.WebGLRenderer(this.screenSize, this.screenSize, {
+		this.renderer = new PIXI.WebGLRenderer(this.screenSize + 1, this.screenSize, {
 			autoResize: true,
 			antialias: true,
 			resolution: window.devicePixelRatio || 1
 		})
 
-		this.renderer.backgroundColor = 0x000000
+		this.renderer.backgroundColor = 0x050505
 		canvasContainer.appendChild(this.renderer.view)
 
 		this.stage = new PIXI.Container()
 		this.world = {}
+		this.cellSize = 0
+		this.worldObject = {}
+		this.renderOffset = {x: 0, y: 0}
+		this.cells = []
 
 		this.draw()
 	}
 
+	setOffset(vOffset) {
+
+	}
+
 	addWorld(worldData, worldLayout) {
 		this.world = worldData
-		this.world.cellSize = this.screenSize / worldData.width
+		this.cellSize = this.screenSize / worldData.width
 
 		let container = new PIXI.Container()
-		let worldObject = this.preprocessWorld(container, worldLayout)
+		this.worldObject = this.preprocessWorld(container, worldLayout)
 
-		this.stage.addChild(worldObject)
+		this.stage.addChild(this.worldObject)
 	}
 
 	preprocessWorld(container, worldData) {
 		for(let c = 0; c < worldData.length; c++) {
 			let cell = worldData[c]
-			let processedCell = this.drawCell(cell)
-			container.addChild(processedCell)
+			this.cells.push(cell)
+			container.addChild(cell.graphic)
 		}
 
 		return container
 	}
 
-	drawCell(cell) {
-		var cs = this.world.cellSize
-		var cellGraphic = new PIXI.Graphics()
-		cellGraphic.beginFill(0x000000, 0)
-		cellGraphic.lineStyle(1, 0xffffff, 1)
+	updateWorld() {
+		//this.worldObject.y += this.renderVOffset
 
-		var bc = this.getBaseCoords(cell.loc)
-
-		cellGraphic.moveTo(bc.x, bc.y)
-		if(cell.walls[0] === 0) cellGraphic.lineTo(bc.x + cs, bc.y)
-
-		cellGraphic.moveTo(bc.x + cs, bc.y)
-		if(cell.walls[1] === 0) cellGraphic.lineTo(bc.x + cs, bc.y + cs)
-
-		cellGraphic.moveTo(bc.x + cs, bc.y + cs)
-		if(cell.walls[2] === 0) cellGraphic.lineTo(bc.x, bc.y + cs)
-
-		cellGraphic.moveTo(bc.x, bc.y + cs)
-		if(cell.walls[3] === 0) cellGraphic.lineTo(bc.x, bc.y)
-
-		cellGraphic.endFill()
-
-		return cellGraphic
-	}
-
-	getBaseCoords(loc) {
-		return {
-			x: this.world.cellSize * loc.x,
-			y: this.world.cellSize * loc.y
+		for(let c = 0; c < this.cells.length; c++) {
+			this.cells[c].draw(this.cellSize)
 		}
 	}
 
+	updatePlayer() {
+
+	}
+
 	draw() {
+		this.updateWorld()
+		this.updatePlayer()
 		this.renderer.render(this.stage)
 		requestAnimationFrame(this.draw.bind(this))
 	}
